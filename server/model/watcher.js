b3f28632716c21d40c5884b7367f777251908c2e
Rob Butcher <robbutcher2001@gmail.com>
2016-09-14 21:58:50 +0100

    Removing obsolete files

--- server/model/watcher.js
+++
@@ -1,5 +0,0 @@
-var watch = require('node-watch');
-
-watch('/home/music-library/tune-library', { recursive: true, followSymLinks: true }, function(filename) {
-  console.log(filename, ' changed.');
-});
--- test.js
+++
@@ -1,5 +0,0 @@
-var watch = require('node-watch');
- 
-watch('/Users/rbutcher/Downloads', { recursive: true, followSymLinks: true }, function(filename) {
-  console.log(filename, ' changed.');
-});
