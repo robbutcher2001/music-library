aa1dd3c03cb23e68919367c4863bd351a6839954
Rob Butcher <robbutcher2001@gmail.com>
2016-09-23 22:32:47 +0100

    Adding React component for artist

--- app/components/ArtistList.jsx
+++ app/components/ArtistList.jsx
@@ -2,9 +2,16 @@ var React = require("react");
 
 module.exports = React.createClass({
    render:function(){
+   var ren = this.props;
        return(
            <div className="butch">
-                hello someone
+                {
+                     this.props.artists.data.artists.map(function(artist,index){
+                         return(
+                             <div key={"artist"+index}>{artist.name}</div>
+                         )
+                     })
+                }
            </div>
        )
    }
--- app/main.jsx
+++ app/main.jsx
@@ -1,7 +1,28 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 var ArtistList = require("./components/ArtistList.jsx");
+var $ = require("jquery");
 
 function render(){
-    ReactDOM.render(<ArtistList />, document.getElementById("container"));
+    getArtists().then(function(artists) {
+        ReactDOM.render(<ArtistList artists={artists} />, document.getElementById("container"));
+    });
+}
+
+render();
+
+function getArtists() {
+  return new Promise(function(resolve, reject) {
+    $.ajax({
+      url: "/music-library/api/artists",
+      type: "GET",
+      dataType: "json",
+      error: function(response) {
+        $('#container').html('Shit went wrong');
+      },
+      success: function(response) {
+        resolve(response);
+      }
+    });
+  });
 }
